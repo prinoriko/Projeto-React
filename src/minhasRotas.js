6804d410d443ff17b600b59e2c6130f96394a283
prinoriko <111386047+prinoriko@users.noreply.github.com>
2023-06-11 23:09:55 -0300

    Update minhasRotas.js

--- src/minhasRotas.js
+++ src/minhasRotas.js
@@ -2,11 +2,9 @@ import { BrowserRouter, Route, Routes} from "react-router-dom";
 
 import Home from "./Home";
 import Atv01 from "./Atv01";
-import Atv02 from "./Atv02";
 import Atv03 from "./Atv03";
 import Atv04 from "./Atv04";
 import Atv05 from "./Atv05";
-import Atv06 from "./Atv06";
 
 export default function MinhasRotas(){
     return (
@@ -14,11 +12,9 @@ export default function MinhasRotas(){
         <Routes>
            <Route path="/" element={<Home />} />
            <Route path="/atv01" element={<Atv01 />} />
-           <Route path="/atv02" element={<Atv02 />} />
            <Route path="/atv03" element={<Atv03 />} />
            <Route path="/atv04" element={<Atv04 />} />
            <Route path="/atv05" element={<Atv05 />} />
-           <Route path="/atv06" element={<Atv06 />} />
         </Routes>     
         </BrowserRouter>
     );
