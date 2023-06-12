e3ed85b1de174ae455dc582964186fc92b58a832
prinoriko <111386047+prinoriko@users.noreply.github.com>
2023-06-12 01:00:32 -0300

    Update Calculadora.js

--- src/Components/Calculadora.js
+++ src/Components/Calculadora.js
@@ -1,50 +1,54 @@
 import React, { useState } from 'react';
 
-const Calculadora = () => {
-  const [valorVisor, setValorVisor] = useState('0');
+function Calculadora() {
+  const [display, setDisplay] = useState('0');
 
-  const handleClick = (valor) => {
-    if (valorVisor === '0') {
-      setValorVisor(valor);
-    } else {
-      setValorVisor(valorVisor + valor);
+  const handleButtonClick = (value) => {
+    let newDisplay = display;
+
+    switch (value) {
+      case 'AC':
+        newDisplay = '0';
+        break;
+      case '=':
+        try {
+          newDisplay = eval(display).toString();
+        } catch (error) {
+          newDisplay = 'Error';
+        }
+        break;
+      case '±':
+        newDisplay = (parseFloat(display) * -1).toString();
+        break;
+      case '⌫':
+        newDisplay = display.slice(0, -1);
+        break;
+      default:
+        newDisplay = display === '0' ? value : display + value;
+        break;
     }
+
+    setDisplay(newDisplay);
   };
 
-  const calcularResultado = () => {
-    try {
-      const resultado = eval(valorVisor);
-      setValorVisor(resultado.toString());
-    } catch (error) {
-      setValorVisor('Erro');
+  const handleDecimalClick = () => {
+    if (!display.includes('.')) {
+      setDisplay(display + '.');
     }
-  };
-
-  const limparVisor = () => {
-    setValorVisor('0');
   };
 
   return (
-    <div className="calculator">
-      <div className="display">{valorVisor}</div>
+    <div className="Calculadora">
+      <div className="display">{displayValue}</div>
       <div className="buttons">
-        <button className="button" onClick={() => handleClick('7')}>7</button>
-        <button className="button" onClick={() => handleClick('8')}>8</button>
-        <button className="button" onClick={() => handleClick('9')}>9</button>
-        <button className="button" onClick={() => handleClick('+')}>+</button>
-        <button className="button" onClick={() => handleClick('4')}>4</button>
-        <button className="button" onClick={() => handleClick('5')}>5</button>
-        <button className="button" onClick={() => handleClick('6')}>6</button>
-        <button className="button" onClick={() => handleClick('-')}>-</button>
-        <button className="button" onClick={() => handleClick('1')}>1</button>
-        <button className="button" onClick={() => handleClick('2')}>2</button>
-        <button className="button" onClick={() => handleClick('3')}>3</button>
-        <button className="button" onClick={() => handleClick('*')}>*</button>
-        <button className="button" onClick={() => handleClick('0')}>0</button>
-        <button className="button" onClick={() => handleClick('.')}>.</button>
-        <button className="button" onClick={() => calcularResultado()}>=</button>
-        <button className="button" onClick={() => handleClick('/')}>/</button>
-        <button className="button" onClick={() => limparVisor()}>C</button>
+        {[7, 8, 9, '+', 4, 5, 6, '-', 1, 2, 3, '*', 0, '.', '=', '/'].map((value) => (
+          <button key={value} className="button" onClick={() => handleButtonClick(value)}>
+            {value}
+          </button>
+        ))}
+        <button className="button" onClick={clearDisplay}>
+          C
+        </button>
       </div>
     </div>
   );
